737cec4183d10308a5602caf740e91b2374103eb
S-Karthick-Raja <kraja2796@gmail.com>
2022-01-25 17:18:10 +0530

    Reducer for Creating Book

---
+++ src/redux/reducers/createBookReducer.js
@@ -0,0 +1,25 @@
+/* eslint-disable default-case */
+
+import { CREATE_BOOK_FAIL, CREATE_BOOK_REQUEST, CREATE_BOOK_SUCCESS } from '../actions/books/actionTypes'
+
+const createBookReducer = (state = {}, action) => {
+    switch (action.type) {
+        case CREATE_BOOK_REQUEST:
+            return {
+                loading: true,
+            };
+        case CREATE_BOOK_SUCCESS:
+            return {
+                book: action.payload,
+            };
+        case CREATE_BOOK_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export { createBookReducer };
