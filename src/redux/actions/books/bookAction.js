55379f5820ac6982cb195f9d093e0cda142f1abc
S-Karthick-Raja <kraja2796@gmail.com>
2022-01-26 23:38:08 +0530

    book action

--- src/redux/actions/books/bookAction.js
+++ src/redux/actions/books/bookAction.js
@@ -1,23 +1,23 @@
 import axios from "axios";
-import { CREATE_BOOK_FAIL, CREATE_BOOK_REQUEST, CREATE_BOOK_SUCCESS } from "./actionTypes";
+import { CREATE_BOOK_FAIL, CREATE_BOOK_REQUEST, CREATE_BOOK_SUCCESS, FETCH_BOOK_REQUEST, FETCH_BOOK_FAIL, FETCH_BOOK_SUCCESS } from "./actionTypes";
 
 const createBookAction = bookData => {
     return async dispatch => {
         try {
-            dispatch ({
+            dispatch({
                 type: CREATE_BOOK_REQUEST,
             });
             const config = {
-                'Content-Type' : 'application/json',
+                'Content-Type': 'application/json',
             };
-    
-            const {data} = await axios.post('/api/books', bookData, config);
-    
+
+            const { data } = await axios.post('/api/books', bookData, config);
+
             dispatch({
                 type: CREATE_BOOK_SUCCESS,
                 payload: data,
             });
-    
+
         } catch (error) {
             dispatch({
                 type: CREATE_BOOK_FAIL,
@@ -28,4 +28,33 @@ const createBookAction = bookData => {
 };
 
 
-export {createBookAction};
+// Fetch Book
+const fetchBooks = () => {
+    return async dispatch => {
+        try {
+            dispatch({
+                type: FETCH_BOOK_REQUEST,
+                loading: true,
+            });
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+            // make http call to our backend
+            const { data } = await axios.get('/api/books', config);
+
+            dispatch({
+                type: FETCH_BOOK_SUCCESS,
+                payload: data,
+            });
+        } catch (error) {
+            dispatch({
+                type: FETCH_BOOK_FAIL,
+                error: error.response && error.response.data.message,
+            });
+        }
+    };
+};
+
+export { createBookAction, fetchBooks };
