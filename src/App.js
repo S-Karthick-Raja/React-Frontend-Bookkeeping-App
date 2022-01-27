26887e312ae9f3a0d44834dd3647e669c4b67653
S-Karthick-Raja <kraja2796@gmail.com>
2022-01-27 19:53:50 +0530

    Homepage Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Navbar } from "./components/Navbar/Navbar";
-import {AddBook} from './components/book/Addbook'
-import {Books} from './components/book/Books'
+import { AddBook } from './components/book/Addbook'
+import { Books } from './components/book/Books'
 import { RegisterUser } from './components/users/RegisterUser';
 import { LoginUser } from './components/users/LoginUser';
+import { HomePage } from './components/HomePage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Switch>
+          <Route exact path='/' component={HomePage} />
           <Route exact path='/books' component={Books} />
           <Route exact path='/addbook' component={AddBook} />
           <Route exact path='/register' component={RegisterUser} />
