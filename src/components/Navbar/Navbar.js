09455585bc94a92386ae8a5e86ad525ffbfe11fd
S-Karthick-Raja <kraja2796@gmail.com>
2022-01-27 19:22:15 +0530

    Private & Public Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,116 +1,79 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
-    return (
-        <header>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-                <Link className='navbar-brand' to='/'>
-                    BooKKeeping Application
-                </Link>
-                <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-toggle='collapse'
-                    data-target='#navbarColor01'
-                    aria-controls='navbarColor01'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'>
-                    <span className='navbar-toggler-icon'></span>
-                </button>
+  const state = useSelector(state => state.userLogin);
 
-                <div className='collapse navbar-collapse' id='navbarColor01'>
-                    <ul className='navbar-nav m-auto'>
-                        <li className='nav-item active'>
-                            <Link className='nav-link' to='/'>
-                                Home <span className='sr-only'>(current)</span>
-                            </Link>
-                        </li>
-                        
-                        {/* List menu items */}
-                        <>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/books'>
-                                    Books
-                                </Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/addbook'>
-                                    Add book
-                                </Link>
-                            </li>
+  const { userInfo, loading, error } = state;
+  return (
+    <header>
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <Link className='navbar-brand' to='/'>
+          BooKkeeping Application
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarColor01'
+          aria-controls='navbarColor01'
+          aria-expanded='false'
+          aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
 
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/users'>
-                                    Users
-                                </Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/login'>
-                                    Logout
-                                </Link>
-                            </li>
-                        </>
-                        {/* Login Register */}
-                        <>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/login'>
-                                    Login
-                                </Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/register'>
-                                    Register
-                                </Link>
-                            </li>
-                        </>
+        <div className='collapse navbar-collapse' id='navbarColor01'>
+          <ul className='navbar-nav m-auto'>
+            <li className='nav-item active'>
+              <Link className='nav-link' to='/'>
+                Home <span className='sr-only'>(current)</span>
+              </Link>
+            </li>
+            {!userInfo ? (
+              <>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/login'>
+                    Login
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/register'>
+                    Register
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/books'>
+                    Books
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/addbook'>
+                    Add book
+                  </Link>
+                </li>
 
-                        {/* Drop dowm */}
-                        {true ? (
-                            <li className='nav-item dropdown'>
-                                <Link
-                                    className='nav-link dropdown-toggle btn-dark'
-                                    data-toggle='dropdown'
-                                    href='/'
-                                    role='button'
-                                    aria-haspopup='true'
-                                    aria-expanded='false'></Link>
-                                <div className='dropdown-menu'>
-                                    <Link className='dropdown-item' to='/profile'>
-                                        Profile
-                                    </Link>
-                                    <Link className='dropdown-item' to='/addbook'>
-                                        Add book
-                                    </Link>
-                                    <Link className='dropdown-item' to='/books'>
-                                        Books
-                                    </Link>
-
-                                    <div className='dropdown-divider'></div>
-                                    <button className='dropdown-item'>Logout</button>
-                                </div>
-                            </li>
-                        ) : (
-                            ''
-                        )}
-                    </ul>
-                    <form className='form-inline my-2 my-lg-0'>
-                        <input
-                            className='form-control mr-sm-2'
-                            type='text'
-                            placeholder='Search'
-                        />
-                        <button className='btn btn-secondary my-2 my-sm-0' type='submit'>
-                            Search
-                        </button>
-                    </form>
-                </div>
-            </nav>
-        </header>
-    );
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/users'>
+                    Users
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/login'>
+                    Logout
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
-
-
-export { Navbar };
+export  {Navbar};
