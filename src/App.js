3c69f594340c468fe5dbfb5a1900ab6edfee7c6b
S-Karthick-Raja <kraja2796@gmail.com>
2022-01-27 15:22:39 +0530

    Register User

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import { Navbar } from "./components/Navbar/Navbar";
 import {AddBook} from './components/book/Addbook'
 import {Books} from './components/book/Books'
+import { RegisterUser } from './components/users/RegisterUser';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path='/books' component={Books} />
           <Route exact path='/addbook' component={AddBook} />
+          <Route exact path='/register' component={RegisterUser} />
         </Switch>
       </BrowserRouter>
 
