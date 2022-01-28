171cb650159a10b03458c4c1994c5b7395d6aea0
S-Karthick-Raja <kraja2796@gmail.com>
2022-01-28 14:13:06 +0530

    error,success,loading, message

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading please wait......</h1>
+    <div className='spinner-border' role='status'>
+      <span className='sr-only'>Loading...</span>
     </div>
   );
 };
 
-export  {Loading};
+export { Loading };
